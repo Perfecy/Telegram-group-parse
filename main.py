c9b9938fd9e2ed9e64b8a4f79634073f3dc0260f
Simon Zohrabyan <simon.zohrabyan@gmail.com>
2022-01-28 00:18:53 +0300

    nihuya ne poluchayetsa

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+import configparser
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+PeerChannel
+)
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# Reading Configs
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Setting configuration values
+api_id = int(config['Telegram']['api_id'])
+api_hash = config['Telegram']['api_hash']
+
+api_hash = str(api_hash)
+
+phone = config['Telegram']['phone']
+username = config['Telegram']['username']
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    client = TelegramClient(username, api_id, api_hash)
+    client.start()
+    print("Client Created")
+    # Ensure you're authorized
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        try:
+            client.sign_in(phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            client.sign_in(password=input('Password: '))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    user_input_channel = input("enter entity(telegram URL or entity id):")
+
+    if user_input_channel.isdigit():
+        entity = PeerChannel(int(user_input_channel))
+    else:
+        entity = user_input_channel
+
+    my_channel = client.get_entity(entity)
+
+    offset_id = 0
+    limit = 100
+    all_messages = []
+    total_messages = 0
+    total_count_limit = 0
+
+    while True:
+        print("Current Offset ID is:", offset_id, "; Total Messages:", total_messages)
+        history = client(GetHistoryRequest(
+            peer=my_channel,
+            offset_id=offset_id,
+            offset_date=None,
+            add_offset=0,
+            limit=limit,
+            max_id=0,
+            min_id=0,
+            hash=0
+        ))
+        if not history.messages:
+            break
+        messages = history.messages
+        for message in messages:
+            all_messages.append(message.to_dict())
+        offset_id = messages[len(messages) - 1].id
+        total_messages = len(all_messages)
+        if total_count_limit != 0 and total_messages >= total_count_limit:
+            break
+
+    with open('messages.json', 'w') as outfile:
+        json.dump(all_messages, outfile)
+
